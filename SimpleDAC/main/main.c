3507b768a6878b03e35a8035d98bc0e18c01cd85
GREIMEL_thomas <thomas.greimel04@gmail.com>
2022-08-05 20:48:46 +0200

    SimplePWM

--- SimpleDAC/main/main.c
+++ SimpleDAC/main/main.c
@@ -1,11 +1,21 @@
+#include <driver/dac_common.h>
+#include <hal/dac_types.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
+#include <sys/_stdint.h>
+#include <sys/unistd.h>
 
 void app_main(void)
 {
+
+	dac_output_enable(DAC_CHANNEL_1);
+
+	printf("Hello from app_main!\n");
+	uint8_t dacval = 0;
+
     while (1) {
-        printf("Hello from app_main!\n");
-        sleep(1);
+        dac_output_voltage(DAC_CHANNEL_1, dacval);
+    	dacval++;
+    	usleep(1);
     }
+
 }
